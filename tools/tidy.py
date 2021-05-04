1f2283fe672159d014edd1d3cee64d6c1d9bd3d1
Benjamin Pelletier <pelletierb@wing.com>
2021-05-04 00:28:48 -0700

    Tidy formatting

--- tools/tidy.py
+++ tools/tidy.py
@@ -4,7 +4,10 @@ yaml = YAML()
 
 files = ('../remoteid/augmented.yaml', '../remoteid/canonical.yaml')
 for fname in files:
-    with open(fname, 'r') as f:
-        api = yaml.load(f)
-    with open(fname, 'w') as f:
-        yaml.dump(api, f)
+    try:
+        with open(fname, 'r') as f:
+            api = yaml.load(f)
+        with open(fname, 'w') as f:
+            yaml.dump(api, f)
+    except IOError:
+        pass
